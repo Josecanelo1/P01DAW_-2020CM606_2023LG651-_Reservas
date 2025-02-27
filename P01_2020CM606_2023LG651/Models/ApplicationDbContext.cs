a0ac4b3603902a74eb6d1c0bb14f97364cb9d36b
Jose Mendoza <mendozamendoza1998@outlook.es>
2025-02-27 17:22:46 -0600

    Creacion de  ApplicationDbContext

---
+++ P01_2020CM606_2023LG651/Models/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P01_2020CM606_2023LG651.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
