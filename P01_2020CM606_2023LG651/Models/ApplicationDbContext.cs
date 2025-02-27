e11872fa8d6fe12605eec2e456bb821d6e28a3b0
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-02-27 17:46:02 -0600

    Agrega DbSets para Usuario, Sucursal, EspacioParque y Reserva en ApplicationDbContext

--- P01_2020CM606_2023LG651/Models/ApplicationDbContext.cs
+++ P01_2020CM606_2023LG651/Models/ApplicationDbContext.cs
@@ -7,3 +7,10 @@ namespace P01_2020CM606_2023LG651.Models
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Sucursal> Sucursales { get; set; }
+        public DbSet<EspacioParque> EspaciosParque { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+    }
+}
